f259385bbbca351f6581480f8255f29aa68557c1
Romain Rochegude <romain.rochegude@gmail.com>
2026-03-22 16:04:26 +0100

    :sparkles: implement no static rule. See #15

---
+++ src/main/java/com/github/roroche/eorules/conditions/predicates/HasCorrectMainModifiers.java
@@ -0,0 +1,56 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 Romain Rochegude
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.github.roroche.eorules.conditions.predicates;
+
+import com.tngtech.archunit.core.domain.JavaMethod;
+import com.tngtech.archunit.core.domain.JavaModifier;
+import org.cactoos.Scalar;
+
+/**
+ * Check if the {@link JavaMethod} has the correct modifiers for a main method.
+ *
+ * @since 0.0.1
+ */
+public final class HasCorrectMainModifiers implements Scalar<Boolean> {
+
+    /**
+     * The {@link JavaMethod} to test.
+     */
+    private final JavaMethod method;
+
+    /**
+     * Ctor.
+     *
+     * @param method The {@link JavaMethod} to test.
+     */
+    public HasCorrectMainModifiers(final JavaMethod method) {
+        this.method = method;
+    }
+
+    @Override
+    public Boolean value() {
+        return this.method.getModifiers().contains(JavaModifier.PUBLIC)
+            && this.method.getModifiers().contains(JavaModifier.STATIC);
+    }
+}
