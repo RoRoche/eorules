01000c51fb159034c6b5a3a1101a16e03125cac1
Romain Rochegude <romain.rochegude@gmail.com>
2026-07-28 21:20:02 +0100

    :recycle: restore synthetic branch. See #123

--- src/main/java/com/github/roroche/eorules/conditions/NotHavePrivateMethods.java
+++ src/main/java/com/github/roroche/eorules/conditions/NotHavePrivateMethods.java
@@ -48,6 +48,8 @@ public final class NotHavePrivateMethods extends ArchCondition<JavaClass> {
         clazz.getMethods().stream().filter(
             (final JavaMethod method) ->
                 method.getModifiers().contains(JavaModifier.PRIVATE)
+                    &&
+                    !method.reflect().isSynthetic()
         ).forEach(
             (final JavaMethod method) ->
                 events.add(
